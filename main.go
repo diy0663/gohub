359e8099156e5d7019fab12ce3862022405c32d3
diy0663 <779893396@qq.com>
2022-02-14 17:19:40 +0800

    引入gin, 注册两个全局中间件,定义一个get  / 的 路由, 指定运行端口为8000

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello gohub--!")
+	r := gin.New()
+	// 注册两个用到的全局中间件
+	r.Use(gin.Logger(), gin.Recovery())
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"key": "value",
+		})
+	})
+	// http://127.0.0.1:8000/ 即可访问
+	// 指定8000端口
+	r.Run(":8000")
 }
