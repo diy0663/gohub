6903ea1feb996c1bdcc8aa6f5d5a1f51cba21b3e
diy0663 <779893396@qq.com>
2022-02-15 11:22:49 +0800

    引入 	github.com/diy0663/go_project_packages v0.0.0-20220215025521-e2463c19afb1 ,完成配置读取使用,但是还不支持指定读取不同环境的.env

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+// main.go 里面调用这个方法, 触发包内其他init方法 执行从而达成载入env配置的目的,载入后才能去读取
+func Initialize() {
+
+}
--- main.go
+++ main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 
 	"github.com/diy0663/gohub/bootstrap"
+
+	"github.com/diy0663/go_project_packages/config"
+	btsConfig "github.com/diy0663/gohub/config"
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
+
 func main() {
+
 	router := gin.New()
 	bootstrap.SetUpRoute(router)
 
 	// 指定8000端口
-	err := router.Run(":8000")
+	err := router.Run(":" + config.GetString("app.port"))
 	if err != nil {
 		// 打印可能出现的错误,例如端口占用
 		fmt.Println(err.Error())
