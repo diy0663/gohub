55f87a0d3e5ed37dd97f7d9a7fb7f3c5fb3827fa
diy0663 <779893396@qq.com>
2022-02-24 15:42:36 +0800

    重构,把 govalidator 用到的重复部分代码抽取出来

---
+++ app/requests/request.go
@@ -0,0 +1,15 @@
+package requests
+
+import "github.com/thedevsaddam/govalidator"
+
+// 把通用的验证方法抽取到这里
+func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid",
+		Messages:      messages,
+	}
+
+	return govalidator.New(opts).ValidateStruct()
+}
