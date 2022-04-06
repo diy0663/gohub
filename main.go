59869a8b10106365a07a0a4ea6958453233dfc8a
diy0663 <779893396@qq.com>
2022-04-06 17:48:22 +0800

    引入初始化配置以及.env配置方式的实现

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
 
+	"github.com/diy0663/go_project_packages/config"
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	btsConfig.Initialize()
+}
 
 func main() {
 
@@ -13,7 +19,7 @@ func main() {
 
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":3000")
+	err := router.Run(":" + config.GetString("app.port"))
 	if err != nil {
 		// 错误打印出来 (例如端口占用)
 		fmt.Println(err.Error())
