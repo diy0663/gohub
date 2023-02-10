6bda9b4e876f8f02a1e46a10d737a5853cd6bf3f
diy0663 <lingege244@139.com>
2023-02-10 17:10:39 +0800

    使用gin 里面的404路由处理方法,mac 不想频繁报防火墙提示,就在gin的启动端口前 把 127.0.0.1  给加上 同时 使用 gin  读取request 里面的header头信息

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"hello": "gin",
+			"hello": "gin from :8000",
 		})
 	})
-	r.Run(":8080")
+
+	// 处理 404 page not found 的无效路由
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"error_code":    404,
+				"errpr_message": " 路由未定义",
+			})
+		}
+	})
+
+	r.Run("127.0.0.1:8000")
 }
