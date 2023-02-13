59c5e1dc4b90c2e364a6b7de004953b336d565af
diy0663 <lingege244@139.com>
2023-02-13 15:35:20 +0800

    新增model 示例

--- main.go
+++ main.go
@@ -26,27 +26,6 @@ func main() {
 	// 连接数据库
 	bootstrap.SetupDB()
 	bootstrap.SetupRoute(r)
-	// r.Use(gin.Logger(), gin.Recovery())
-
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"hello": "gin from :8000",
-	// 	})
-	// })
-
-	// // 处理 404 page not found 的无效路由
-	// r.NoRoute(func(c *gin.Context) {
-	// 	acceptString := c.Request.Header.Get("Accept")
-	// 	if strings.Contains(acceptString, "text/html") {
-	// 		c.String(http.StatusNotFound, "页面返回 404")
-	// 	} else {
-	// 		c.JSON(http.StatusOK, gin.H{
-	// 			"error_code":    404,
-	// 			"error_message": " 路由未定义",
-	// 		})
-	// 	}
-	// })
-
 	// mac M1环境下 用 127.0.0.1:3000 就不会频繁提示防火墙了
 	err := r.Run(config.Get("app.mac_m1") + ":" + config.Get("app.port"))
 	if err != nil {
