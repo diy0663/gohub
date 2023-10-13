23992bd82e6c48ec8fd0ea19a12bd0db240ba29f
diy0663 <779893396@qq.com>
2023-10-13 15:35:18 +0800

    引入gin

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello gohub!")
+	// cmd+shift+K 用于vscode 删除 单行
+	// 自行配置 cmd+D 用于复制单行
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Hello": "World"})
+	})
+
+	r.Run()
 }
