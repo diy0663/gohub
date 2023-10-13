6335bcba9f6c3b2a8ca5bce5e28f20968edeeb84
diy0663 <779893396@qq.com>
2023-10-13 15:57:58 +0800

    改写gin 引擎的一个new 方式

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,11 +10,28 @@ import (
 func main() {
 	// cmd+shift+K 用于vscode 删除 单行
 	// 自行配置 cmd+D 用于复制单行
+	r := gin.New()
+	// 中间件
+	r.Use(gin.Logger(), gin.Recovery())
 
-	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"Hello": "World"})
+		c.JSON(http.StatusOK, gin.H{"hello": "Gin"})
 	})
 
-	r.Run()
+	// 专门对应404 的路由
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "查无该页面")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "请检查请求路由",
+			})
+		}
+	})
+
+	//  http://127.0.0.1:8080
+	r.Run(":8080")
+
 }
