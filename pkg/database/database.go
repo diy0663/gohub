7f8f7a05cb738666d1365c7ec48b5b6e9096535f
diy0663 <779893396@qq.com>
2023-11-27 16:43:00 +0800

    新增一个gorm的链接初始化使用写法

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
 )
@@ -17,6 +18,7 @@ var SQLDB *sql.DB
 // 其实最主要就是使用 gorm.Open
 func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
 	var err error
+
 	DB, err = gorm.Open(dbConfig, &gorm.Config{
 		SkipDefaultTransaction:                   false,
 		NamingStrategy:                           nil,
@@ -57,4 +59,23 @@ func TableNameByStruct(obj interface{}) string {
 	stmt := &gorm.Statement{DB: DB}
 	stmt.Parse(obj)
 	return stmt.Schema.Table
+}
+
+// 另一种数据库链接的写法,外层可以在最开始的时候初始化,定义全局变量配合sync.Once 使用
+func MustNewGormDB(dsn string, opts ...gorm.Option) (*gorm.DB, *sql.DB) {
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+	db, err := gorm.Open(mysql.Open(dsn), opts...)
+	if err != nil {
+		panic(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	err = sqlDB.Ping()
+	if err != nil {
+		panic(err)
+	}
+	// todo 在这里暂时不设置最大连接数,最大空闲连接数,链接连接时间
+	return db, sqlDB
 }
